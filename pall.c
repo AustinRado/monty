9bd84bf53a38131b5ef110fde21029a702e50e93
AustinRado <radoaussy@gmail.com>
2023-08-24 15:47:26 -0700

    changes

--- pall.c
+++ pall.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/**
+ *pall - prints elements of the stack
+ *@stack: pointer to the stack
+ *@line_number: unused variable
+ */
+
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = *stack;
--- push.c
+++ push.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/**
+ * push- add an element on top of stack
+ * @stack: stack structure
+ * @value: Element to add on top of stack
+ */
+
 void push(stack_t **stack, unsigned int value)
 {
 	stack_t *new_node = malloc(sizeof(stack_t));
