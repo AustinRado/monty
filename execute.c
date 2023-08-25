4522063eb86dcaa925474f98316e37fd94a11183
AustinRado <radoaussy@gmail.com>
2023-08-24 17:36:24 -0700

    nop

--- execute.c
+++ execute.c
@@ -74,6 +74,10 @@ void exe_instruction(FILE *file)
 		{
 			add(&stack, line_number);
 		}
+		else if (strcmp(opcode, "nop") == 0)
+		{
+			nop(&stack, line_number);
+		}
 		else
 		{
 			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
